01a53ca6cd57563b4a419e6123a485ff8dba8c03
Nothing07 <matt.scott@live.com.au>
2015-08-13 19:35:40 +1000

    fixed bugs

--- src/GameLogic/Snap.cs
+++ src/GameLogic/Snap.cs
@@ -141,26 +141,27 @@ namespace CardGames.GameLogic
 		{
 			//TODO: consider deducting score for miss hits???
 
-			if ( player >= 0 && player < _score.Length &&  	// its a valid player
-				 IsStarted && 								// and the game is started
-				 _topCards [0] != null && _topCards [0].Rank == _topCards [1].Rank) // and its a match
-			{
-				_score[player]++;
-				_gameTimer.Stop();
 			if (player >= 0 && player < _score.Length && // its a valid player
 			    IsStarted && // and the game is started
-			    _topCards [0] != null && _topCards [0].Rank == _topCards [1].Rank) 
-			{ // and its a match
-				_score [player]++;
-				add - player - hit;
-				//TODO: consider playing a sound here...
-			} else if (player >= 0 && player < _score.Length) 
+			    _topCards [0] != null && _topCards [0].Rank == _topCards [1].Rank) // and its a match
 			{
-				_score [player]--;
-			}
+				_score [player]++;
+				_gameTimer.Stop ();
+				if (player >= 0 && player < _score.Length && // its a valid player
+				   IsStarted && // and the game is started
+				   _topCards [0] != null && _topCards [0].Rank == _topCards [1].Rank)
+				{ // and its a match
+					_score [player]++;
+					//TODO: consider playing a sound here...
+				}
+				else if (player >= 0 && player < _score.Length)
+				{
+					_score [player]--;
+				}
 
-			// stop the game...
-			_started = false;
+				// stop the game...
+				_started = false;
+			}
 		}
 	
 		#region Snap Game Unit Tests
